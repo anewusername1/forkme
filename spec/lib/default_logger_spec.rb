8e10111b6f6b353a951762852697944b76399bb9
teubanks <traceye@pmamediagroup.com>
2010-10-21 11:58:36 -0600

    created some tests for default logger

---
+++ spec/lib/default_logger_spec.rb
@@ -0,0 +1,39 @@
+require File.expand_path(File.join('..', '/spec_helper.rb'), File.dirname(__FILE__))
+require 'stringio'
+
+# sio = StringIO.new
+# old_stdout, STDOUT = STDOUT, sio
+
+
+describe 'DefaultLogger' do
+  before(:all) do
+    @sio = StringIO.new
+    @old_stdout, STDOUT = STDOUT, @sio
+  end
+
+  describe "#error" do
+    it "should print error" do
+      DefaultLogger.error "erred"
+      @sio.string.should == "erred\n"
+    end
+  end
+  
+  describe "#info" do
+    it "should print some info" do
+      DefaultLogger.info "infoed"
+      @sio.string.should == "infoed\n"
+    end
+  end
+  
+  describe "#debug" do
+    it "should print some debug info" do
+      DefaultLogger.debug "debugged"
+      @sio.string.should == "debugged\n"
+    end
+  end
+
+  after(:all) do
+    STDOUT = @old_stdout # restore stdout
+  end
+end
+
---
+++ spec/spec_helper.rb
@@ -0,0 +1,1 @@
+require File.expand_path(File.join("..", "lib", "loompa.rb"), File.dirname(__FILE__))
